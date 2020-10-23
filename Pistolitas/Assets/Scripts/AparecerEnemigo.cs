862be6c371fb88014f490a1e5037e85c0bb5ca75
Marynadi Cornejo <marynadi07@gmail.com>
2020-10-22 20:20:26 -0500

    Proyecto Listo
    El proyecto de PC1

---
+++ Pistolitas/Assets/Scripts/AparecerEnemigo.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+using TMPro;
+
+public class AparecerEnemigo : MonoBehaviour
+{
+
+    public float maximoEnemigos;
+
+    float contador = 15;
+
+    public GameObject enemigo;
+
+    public GameObject AvisoInicio;
+
+    public GameObject AvisoFinal;
+
+    public TextMeshProUGUI texto;
+
+    float puntaje;
+
+    public static AparecerEnemigo instancia = null;
+
+    AudioSource source;
+
+    public AudioClip[] sonidos;
+
+    private void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+        }else if(instancia != gameObject)
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+
+    void Start()
+    {
+        contador = maximoEnemigos;
+        source = GetComponent<AudioSource>();
+        puntaje = 0;
+        texto.text = puntaje.ToString();
+        StartCoroutine(AparicionObjetos());
+        
+    }
+
+    
+    public void Terminar()
+    {
+        AvisoFinal.SetActive(true);
+        source.PlayOneShot(sonidos[2]);
+
+    }
+    public void AumentarPuntaje()
+    {
+        source.PlayOneShot(sonidos[1]);
+        puntaje++;
+        texto.text = puntaje.ToString();
+
+        if(puntaje == maximoEnemigos)
+        {
+            Terminar();
+        }
+    }
+
+    IEnumerator AparicionObjetos()
+    {
+        source.PlayOneShot(sonidos[0]);
+        yield return new WaitForSeconds(1f);
+        AvisoInicio.SetActive(false);
+        while (contador > 0)
+        {
+            contador--;
+            LeanPool.Spawn(enemigo,new Vector3(Random.Range(-6,6),Random.Range(-2.5f, 2.5f)),new Quaternion(0,0,0,0));
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Pistolitas/Assets/Scripts/MatarEnemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+
+public class MatarEnemigo : MonoBehaviour
+{
+    //public AparecerEnemigo aparecer;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    private void OnMouseDown()
+    {
+        AparecerEnemigo.instancia.AumentarPuntaje();
+        //aparecer.AumentarPuntaje();
+        LeanPool.Despawn(gameObject);
+        
+    }
+
+
+}
